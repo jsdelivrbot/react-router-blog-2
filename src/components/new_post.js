e3b64c0b5f04626be06c0d50793015a54d966dea
Rachna Sidana <rachnasidana28feb@gmail.com>
2017-07-05 01:33:50 -0500

    add create new blog post form

--- src/components/new_post.js
+++ src/components/new_post.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
+import { Field , reduxForm } from 'redux-form';
 
 class NewPost extends Component{
+
+  renderInputField(field){
+    return(
+      <div className="form-group">
+      <label>{field.label}</label>
+        <input className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+
+    );
+  }
+  
+  renderContentField(field){
+    return(
+      <div className="form-group">
+      <label>{field.label}</label>
+        <input className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render(){
     return(
       <div>
-        Hi from NewPost
+        <form>
+          <Field
+            label="Title"
+            name="title"
+            component={this.renderInputField}
+            />
+          <Field
+            label="Tags"
+            name="tags"
+            component={this.renderInputField}
+            />
+            <Field
+              label="Post Content"
+              name="content"
+              component={this.renderContentField}
+              />
+
+        </form>
       </div>
     );
   }
 }
 
-export default NewPost;
+// can have multiple forms in here
+export default reduxForm({
+  form:'PostsNewForm'
+})(NewPost);
