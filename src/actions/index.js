881a02af66ded11167b6b1f24c6eff29eb147516
Rachna Sidana <rachnasidana28feb@gmail.com>
2017-07-06 01:43:18 -0500

    implement create new post lifecycle

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,26 @@
 import axios from 'axios';
+
 export const FETCH_POSTS = 'fetch_posts';
+export const NEW_POST = 'new_post';
+
 const ROOT_URL='http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=rachna';
+const requestUrl=`${ROOT_URL}/posts/${API_KEY}`;
 
 
 export function fetchAllPosts(){
-  const fetchUrl=`${ROOT_URL}/posts/${API_KEY}`;
-  const promise = axios.get(fetchUrl);
+
+  const promise = axios.get(requestUrl);
   return{
     type: FETCH_POSTS,
+    payload: promise
+  };
+}
+
+export function createNewPost(values){
+  const promise = axios.post(requestUrl, values);
+  return{
+    type: NEW_POST,
     payload: promise
   };
 }
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,11 +1,14 @@
 import _ from 'lodash';
 import {FETCH_POSTS} from '../actions/index';
+import {NEW_POST} from '../actions';
 
 export default function(state={}, action){
 
   switch(action.type){
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
+    case NEW_POST:
+      console.log(action.payload);
     default:
       return state;
   }
